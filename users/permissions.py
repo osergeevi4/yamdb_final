34dc63c2106b3a105acc7bb324e8ec9b284bed15
Oleg Konovalov <okodit@yandex.ru>
2021-02-10 21:54:17 +0300

    1

--- users/permissions.py
+++ users/permissions.py
@@ -35,6 +35,6 @@ class IsAdminOrModer(BasePermission):
         if request.user.is_anonymous:
             return False
         return (
-                obj.author == request.user
-                or request.user.role == User.UserRole.ADMIN
-                or request.user.role == User.UserRole.MODERATOR)
+            obj.author == request.user
+            or request.user.role == User.UserRole.ADMIN
+            or request.user.role == User.UserRole.MODERATOR)
